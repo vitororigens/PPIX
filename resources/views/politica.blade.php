6b968cf4a532ffb5084f2de80f4ca2acc84b6327
vitororigens <vitorabreufjb@hotmail.com>
2024-01-23 17:22:04 -0300

    banco atualizado

---
+++ resources/views/politica.blade.php
@@ -0,0 +1,93 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SOS POLITICA DE PRIVACIDADE</title>
+    <meta name="author" content="Xmartlabs" />
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="css/custom.css" rel="stylesheet" type="text/css" />
+
+
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Projecthub" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:title" content="Sos politica de privacidade" />
+    <meta property="og:description" content="Manage your Github projects from the palm of your hand." />
+  </head>
+
+  <body id="terms-and-conditions">
+    <header class="header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1">
+            <a href="/">
+              <img class="img-responsive logo" src="images/projecthub-logo.png" srcset="images/projecthub-logo@2x.png 2x, images/projecthub-logo@3x.png 3x" draggable="false" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="container-fluid">
+      <div class="row terms-and-conditions-section">
+        <div class="col-xs-12 text-center">
+          <h1 class="title">Termos e Condições</h1>
+        </div>
+
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+          <div class="content">
+                            <h2 class=''>Política de privacidade</h2>
+Todas as suas informações pessoais recolhidas serão usadas para ajudar a tornar a sua visita no nosso site e aplicativo uma experiência mais agradável possível e para nos ajudar a atender da melhor forma todos os interessados em nosso conteúdo.<br>
+A garantia da confidencialidade dos dados pessoais dos utilizadores do nosso site é muito importante para nós<br>
+Todas as informações pessoais relativas a clientes e/ou visitantes que usem a plataforma do nosso site ppp.app.br serão tratadas em concordância com a Lei Geral de Proteção de Dados Pessoais (LGPD ou LGPDP), Lei nº 13.709/2018.<br>
+As informações pessoais recolhidas podem incluir seu nome, e-mail, número de telefone, cidade, estado e/ou números de documentos pessoais, caso seja necessário informá-los.<br>
+O uso de nosso aplicativo PPP-S.O.S pressupõe a aceitação deste Acordo de privacidade.<br>
+A equipe da PPP-S.O.S reserva-se ao direito de alterar este acordo sem aviso prévio. Assim, recomendamos que consulte a nossa política de privacidade com regularidade de forma a estar sempre atualizado.<br>
+<h2 class=''>SEÇÃO 1 - Recursos de Rastreio</h2><br>
+O Rastreamento utiliza a coleta dados de localização para permitir o rastreamento do seu smartphone de modo On-line ou Off-line, ou ainda mesmo quando o aplicativo está fechado ou for a de uso.<br>
+   <h2 class=''>SEÇÃO 2 - O QUE FAREMOS COM ESTA INFORMAÇÃO?</h2>
+<br>
+Os dados de latitude, longitude e velocidade são enviados ao sistema de rastreamento para que você mesmo ou outro usuário devidamente liberado passa acompanhar seu percurso em caso de eventual emergência.<br>
+Mesmo que o seu celular esteja com o aplicativo fechado e/ou fora da área de cobertura GSM, o aparelho enviará os mesmos dados na próxima conexão disponível.<br>
+  <h2 class=''>SEÇÃO 3 - CONSENTIMENTO</h2>
+<br>
+Quando você fornece informações pessoais como nome, telefone, localização e endereço, após a realização de ações entendemos que você está de acordo com a coleta de dados para serem utilizados pela nossa empresa.<br>
+E caso você queira retirar seu consentimento, como proceder?<br>
+Se após você nos fornecer seus dados, você mudar de ideia, você pode retirar o seu consentimento para que possamos entrar em contato, para a coleção de dados contínua, uso ou divulgação de suas informações, a qualquer momento, entrando em contato conosco em contato@ppp.app.br.<br>
+Podemos divulgar suas informações pessoais caso sejamos obrigados pela lei para fazê-lo ou se você violar nossos Termos de Serviços.<br>
+  <h2 class=''>SEÇÃO 4 - SERVIÇOS DE TERCEIROS</h2>
+<br>
+Não vendemos, divulgamos ou disponibilizamos dados coletados a terceiros ou a parceiros, todos os dados apenas podem ser consultados no nosso sistema ou em busca vai integração com os devidos acessos restritos e seguros com SSL.<br>
+A nossa política de privacidade não é aplicada a sites de terceiros, por isso, caso visite outro site a partir do nosso deverá ler a política de privacidade do mesmo. Não nos responsabilizamos pela política de privacidade ou conteúdo presente nesses mesmos sites.<br>
+  <h2 class=''>SEÇÃO 5 - SEGURANÇA</h2>
+<br>
+Para proteger suas informações pessoais, tomamos precauções razoáveis e seguimos as melhores práticas da indústria para nos certificar que elas não serão perdidas inadequadamente, usurpadas, acessadas, divulgadas, alteradas ou destruídas.<br>
+&nbsp;Embora nenhum método de transmissão pela Internet ou armazenamento eletrônico é 100% seguro, nós seguimos todos os requisitos da PCI-DSS e implementamos padrões adicionais geralmente aceitos pela indústria.<br>
+  <h2 class=''>SEÇÃO 6 - SISTEMA DE APLICATIVO</h2>
+<br>
+Nosso sistema respeita a privacidade e sigilo total das informações compartilhadas pelo aplicativo ou nosso próprio hardware, todas as informações trafegam com SSL/HTTPS seguro e estão conforme as leis locais de tratamento de dados.<br>
+Estas informações ficam armazenadas em local seguro e não são divulgadas ao público geral, apenas aos usuários que contratam a plataforma, com seus devidos acessos restritos.<br>
+  <h2 class=''>SEÇÃO 7 - ALTERAÇÕES PARA ESSA POLÍTICA DE PRIVACIDADE</h2>
+<br>
+Reservamos o direito de modificar essa política de privacidade a qualquer momento, então por favor, revise-a com frequência. Alterações e esclarecimentos vão surtir efeito imediatamente após sua publicação. Se fizermos alterações de materiais para essa política, iremos notificá-lo aqui que eles foram atualizados, para que você tenha ciência sobre quais informações coletamos, como as usamos, e sob que circunstâncias, se alguma, usamos e/ou divulgamos elas.<br>
+Se nossa loja for adquirida ou fundida com outra empresa, suas informações podem ser transferidas para os novos proprietários para que possamos continuar a vender produtos para você.<br>
+
+  <h2 class=''>Seção 8 - Formulários</h2>
+<br>
+Tal como outros websites, coletamos e utilizamos informação fornecida nos formulários, incluindo o seu endereço IP (Internet Protocol), dados pessoais que você disponibilize (CPF, RG, Endereço completo com CEP e outras informações que se façam necessárias para efetivar seu cadastro e/ou contato, além do tempo da sua visita e que páginas visitou dentro do nosso aplicativo.<br>
+  <h2 class=''>Seção 9 - Cookies</h2>
+<br>
+Usamos cookies para oferecer uma melhor experiência de navegação, analisar o tráfego do site, publicidade, personalizar o conteúdo e para redirecionamento da página. O tratamento ocorrerá com base no Consentimento, sendo que não compartilhamos as informações coletadas com terceiros para finalidade diversa. Para saber mais sobre os dados coletados pelos cookies acesse nossa Política de Privacidade.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
