85bd3ee8d768d244a9c5da918cf847a29884d658
adminplaymovel <admin@playmovel.com.br>
2024-03-22 10:03:38 -0300

    update /

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/politica-de-privacidade', function () {
     return view('politica');
 });
